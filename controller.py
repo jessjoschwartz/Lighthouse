3eee5e937264546895f6bd8662d9fd6a25cef809
Jess Schwartz <jessica.j.schwartz@gmail.com>
2014-07-15 19:01:32 -0700

    Functional login button with error message

--- controller.py
+++ controller.py
@@ -12,24 +12,24 @@ app.config.from_object(__name__)
 def display_start():
     return render_template("start.html")
 
-@app.route("/", methods=["POST"])
-def login():
-    email = request.form['email']
+### User login
+@app.route("/login", methods=["GET"])
+def user_login_get():
+    return render_template("user_login.html")
+
+@app.route("/login", methods=["POST"])
+def user_login_post():
+    username = request.form['username']
     password = request.form['password']
 
     try:
         user = db_session.query(User).filter_by(email=email, password=password).one()
     except:
         flash("Invalid username or password", "error")
-        return redirect(url_for("login"))
+        return redirect(url_for("user_login_get"))
 
     session['user_id'] = user.id
-    return redirect(url_for("display_search"))
-
-### User login
-@app.route("/login", methods=["GET"])
-def user_login():
-    return render_template("user_login.html")
+    return redirect(url_for("traveler_view"))
 
 ### Registration page #1
 @app.route("/register/1", methods=["GET"])
@@ -67,7 +67,7 @@ def display_register_2():
     db_session.commit()
 
     # Redirect user to landing page
-    return redirect(url_for("traveler_trip_request"))
+    return redirect(url_for("traveler_view"))
 
 ### Logout
 @app.route("/logout")
@@ -75,12 +75,17 @@ def logout():
     del session['user_id']
     return redirect(url_for("login"))
 
+# ### Traveler view
+# @app.route("/traveler_view", methods=["GET"])
+# def register():
+#     return render_template("traveler_view.html")
+
 ### End class declarations
 
 def create_db():
     Base.metadata.create_all(engine)
 
-def connect(db_uri="sqlite:///ratings.db"):
+def connect(db_uri="sqlite:///users.db"):
     global engine
     global session
     engine = create_engine(db_uri, echo=False) 
